f949dca3e01f62561867080f045d15c699c14a00
Juan Manuel García <35479220+jmanuelgar@users.noreply.github.com>
2026-06-02 21:27:50 -0600

    Create panel_info.php

---
+++ cont/panel_info.php
@@ -0,0 +1,162 @@
+
+<p style="text-align: justify; margin-right: 10px;"> Este mapa interactivo permite visualizar y conocer todos los atributos de las
+capas de información geográfica de elaboración propia y otras proveniente de diversas
+fuentes. Además, aporta aspectos generales para el análisis del impacto de las inundaciones. La representación espacial de estos aspectos fue la base
+que nos permitió construir esta visualización cartográfica. </p>
+
+<p style="text-align: justify; margin-right: 10px;">El mapa cuenta con los siguientes cuatro elementos de interacción definidos de tal forma
+que posibilitan la visualización y el contenido de cada una de las capas de información
+geográfica: </p>
+
+<ol type="1">
+  <li style="text-align: justify;margin-right: 20px;"> <b>Desplazamiento:</b> permite moverse por todo el espacio representado y más allá de los
+límites administrativos de la demarcación utilizando el ratón como guía.</li>
+<br>
+  <li style="text-align: justify;margin-right: 20px;"> <b>Acercamiento o alejamiento:</b> esta acción propicia el análisis de manera mucho más
+cercana de una zona específica o alejarse lo suficiente para tener
+una perspectiva más amplia de determinadas capas en una región en particular.</li>
+<br>
+  <li style="text-align: justify;margin-right: 20px;"> <b>Control de capas:</b> este elemento facilita tener mayor interacción con el
+mapa mediante la activación o la desactivación de capas de información geográfica. De
+esta manera se evita tener un problema de la saturación de información para así tener
+una lectura espacial más clara sobre la región o sobre una zona determinada.</li>
+<br>
+  <li style="text-align: justify;margin-right: 20px;"> <b>Consultar información </b>: este tipo de interaccción nos permite
+conocer los atributos de cada capa de información geográfica de este mapa. Al
+momento de darle un clic encima de algún objeto espacial de una capa en específico se
+despliega una ventana que muestra el contenido de éste. Estas consultas nos ayudan
+a explorar los datos espaciales y la información geográfica. La información que se puede consultar en este mapa interactivo es la siguiente:
+
+	<br>
+	<br>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col" style="text-align: center;">Capa</th>
+      <th scope="col" style="text-align: center;">Información</th>
+    </tr>
+  </thead>
+  <tbody>
+   <tr>
+    <td> Cuenca </td>
+    <td>
+	<ol type="1">
+	<li> Nombre </li>
+	</ol>
+  </td>
+   </tr>
+   <tr>
+	<td> Municipios </td>
+	<td>
+	<ol type="1">
+        <li> Nombre </li>
+	</ol>
+	</td>
+   </tr>
+   <tr>
+   <td> Cuerpos de agua </td>
+	<td>
+	<ol type="1">
+	<li> Nombre </li>
+	<li> Área </li>
+	<li> Corriente </li>
+	<li> Responsable </li>
+	</ol>
+	</td>
+   </tr>
+   <tr>
+	<td> Presas</td>
+	<td>
+	<ol type="1">
+	<li> Nombre</li>
+	<li> Responsable </li>
+  <li> Estado </li>
+  <li> Alcaldía o municipio </li>
+  <li> Propósito </li>
+  <li> Corriente </li>
+  <li> Altura máxima </li>
+  <li> Nivel de Aguas Máximas Ordinarias </li>
+	</ol>
+	</td>
+   </tr>
+      <tr>
+   <td> Cauces principales </td>
+	<td>
+	<ol type="1">
+	<li> Nombre </li>
+	<li> Tipo </li>
+	<li> Recibe de </li>
+	<li> Descarga a </li>
+	</ol>
+	</td>
+   </tr>
+   <tr>
+	<td> Plantas de bombeo</td>
+	<td>
+	<ol type="1">
+	<li> Nombre</li>
+	<li> Responsable </li>
+  <li> Capacidad </li>
+  <li> Recibe de </li>
+  <li> Descarga a  </li>
+	</ol>
+	</td>
+   </tr>
+   <tr>
+	<td> Túneles</td>
+	<td>
+	<ol type="1">
+	<li> Nombre</li>
+	<li> Tipo </li>
+  <li> Responsable </li>
+  <li> Profundidad </li>
+  <li> Longitud  </li>
+  <li> Diámetro  </li>
+    <li> Capacidad  </li>
+  <li> Fecha de inicio de construcción  </li>
+  <li> Fecha de fin de construcción  </li>
+	</ol>
+	</td>
+   </tr>
+   <tr>
+	<td> Sistema de derivación combinada</td>
+	<td>
+	<ol type="1">
+	<li> Nombre</li>
+	<li> Tipo </li>
+  <li> Responsable </li>
+  <li> Longitud  </li>
+  <li> Diámetro  </li>
+    <li> Capacidad  </li>
+  <li> Recibe de  </li>
+  <li> Descarga a  </li>
+	</ol>
+	</td>
+   </tr>
+   <tr>
+	<td> Lumbreras</td>
+	<td>
+	<ol type="1">
+	<li> Nombre</li>
+	<li> Túnel </li>
+  <li> Altitud </li>
+  <li> Sección  </li>
+  <li> Profundidad  </li>
+	</ol>
+	</td>
+   </tr>
+   <tr>
+	<td> Historias</td>
+	<td>
+	<ol type="1">
+	<li> Nombre</li>
+	<li> Municipio </li>
+  <li> Estado </li>
+  <li> Descripción  </li>
+	</ol>
+	</td>
+   </tr>
+  </tbody>
+  </table>
+ </li>
+</ol>
